98c50f87cac040f661057fb40b10683cf2d4c2a7
Gulsher <gulsherkhan0005@gmail.com>
2021-10-22 23:18:55 +0530

    code pushed

---
+++ App.js
@@ -0,0 +1,33 @@
+//import liraries
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { requestUserPermission, notificationListener } from './src/helper/notificationServices';
+
+
+// create a component
+const App = () => {
+
+  useEffect(() => {
+    requestUserPermission()
+    notificationListener()
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default App;
---
+++ android/app/src/main/java/com/notification/MainActivity.java
@@ -0,0 +1,59 @@
+package com.notification;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.ContentResolver;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.core.app.NotificationCompat;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected  void onCreate(Bundle savedInstanceState){
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel notificationChannel = new NotificationChannel("sound_channel", "Notification", NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setShowBadge(true);
+      notificationChannel.setDescription("");
+      AudioAttributes att = new AudioAttributes.Builder()
+              .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+              .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+              .build();
+      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/ring_bell"), att);
+      notificationChannel.enableVibration(true);
+      notificationChannel.setVibrationPattern(new long[]{400, 400});
+      notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(notificationChannel);
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel notificationChannel = new NotificationChannel("sound_channel2", "Notification", NotificationManager.IMPORTANCE_HIGH);
+      notificationChannel.setShowBadge(true);
+      notificationChannel.setDescription("");
+      AudioAttributes att = new AudioAttributes.Builder()
+              .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+              .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
+              .build();
+      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/ring_bell2"), att);
+      notificationChannel.enableVibration(true);
+      notificationChannel.setVibrationPattern(new long[]{400, 400});
+      notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(notificationChannel);
+    }
+    super.onCreate(savedInstanceState);
+  }
+  @Override
+  protected String getMainComponentName() {
+    return "Notification";
+  }
+}
